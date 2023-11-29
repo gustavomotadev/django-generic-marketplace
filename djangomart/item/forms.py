615d5ada2e4126dc5f178964be9f83492bc18f1b
Mota <mota@debian.mota>
2023-11-29 14:19:08 -0300

    Edit item

--- djangomart/item/forms.py
+++ djangomart/item/forms.py
@@ -27,3 +27,27 @@ class NewItemForm(forms.ModelForm):
             })
 
         }
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'image', 'is_sold')
+
+        widgets = {
+            'name': forms.TextInput({
+                'class': _custom_input_class
+            }),
+            'description': forms.Textarea({
+                'class': _custom_input_class
+            }),
+            'price': forms.TextInput({
+                'class': _custom_input_class
+            }),
+            'image': forms.FileInput({
+                'class': _custom_input_class
+            }),
+            'is_sold': forms.CheckboxInput({
+                'class': 'w-5 h-5 ml-3'
+            })
+
+        }
--- djangomart/item/views.py
+++ djangomart/item/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -42,3 +42,26 @@ def delete(request, pk):
     item.delete()
 
     return redirect('dashboard:index')
+
+@login_required
+def edit(request, pk):
+
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            # do not commit yet so that we can fill the created_by field
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+        
+    #else
+
+    form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item',
+    })
